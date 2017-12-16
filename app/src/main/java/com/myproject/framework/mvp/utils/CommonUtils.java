0d29eea5c3c4b618b8d702d77e30b9edb73958e7
Nhat <nhat292@gmail.com>
2017-12-16 10:07:17 +0700

    Update

--- app/src/main/java/com/myproject/framework/mvp/utils/CommonUtils.java
+++ app/src/main/java/com/myproject/framework/mvp/utils/CommonUtils.java
@@ -27,7 +27,9 @@ import com.myproject.framework.mvp.R;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.regex.Matcher;
@@ -91,5 +93,82 @@ public final class CommonUtils {
 
     public static String getTimeStamp() {
         return new SimpleDateFormat(AppConstants.TIMESTAMP_FORMAT, Locale.US).format(new Date());
+    }
+
+    public static boolean isNotAllowNumber(String str) {
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) return false;
+        }
+        return true;
+    }
+
+    public static String formatMoney(String money) {
+        DecimalFormat formatter = new DecimalFormat("#,###,###");
+        return "Rp " + formatter.format(Double.valueOf(money));
+    }
+
+    public static String formatAccountNumber(String account) {
+        char[] accountArr = account.toCharArray();
+        return "" + accountArr[0] + accountArr[1] + accountArr[2]
+                + "-" + accountArr[3] + accountArr[4] + accountArr[5]
+                + "-" + accountArr[6] + accountArr[7] + accountArr[8];
+    }
+
+    public static String showTime(String time) {
+        SimpleDateFormat fmt = new SimpleDateFormat("h:mm a");
+        String dateString = fmt.format(new Date(Long.valueOf(time)));
+        return dateString;
+    }
+
+    public static String showTime(Date date) {
+        SimpleDateFormat fmt = new SimpleDateFormat("h:mm a");
+        String dateString = fmt.format(date);
+        return dateString;
+    }
+
+    public static String showDate(Date date) {
+        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+        String dateString = fmt.format(date);
+        return dateString;
+    }
+
+    public static String showDateTime(Date date) {
+        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy h:mm a");
+        String dateString = fmt.format(date);
+        return dateString;
+    }
+
+    public static String getDateFormat(Date date) {
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String dateString = fmt.format(date);
+        return dateString;
+    }
+
+    public static boolean isYesterday(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        int curDay = calendar.get(Calendar.DAY_OF_YEAR);
+        calendar.setTime(date);
+        int day = calendar.get(Calendar.DAY_OF_YEAR);
+        return day == curDay - 1;
+    }
+
+    public static boolean isToday(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        int curDay = calendar.get(Calendar.DAY_OF_YEAR);
+        calendar.setTime(date);
+        int day = calendar.get(Calendar.DAY_OF_YEAR);
+        return day == curDay;
+    }
+
+    public static String formatTimeStamp(long timeStamp) {
+        // Create a DateFormatter object for displaying date in specified format.
+        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm a");
+
+        // Create a calendar object that will convert the date and time value in milliseconds to date.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timeStamp);
+        return formatter.format(calendar.getTime());
     }
 }
